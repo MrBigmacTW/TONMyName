6a96743ecd3a05b6df1eb0a3c29e52aaed0e1360
unknown <tp6m4tp6@gmail.com>
2025-04-09 01:34:32 +0800

    Update TON Connect configuration

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import { TonConnectUIProvider } from '@tonconnect/ui-react'
 import App from './App'
 import './index.css'
 
+// 配置 manifest URL
+const manifestUrl = `https://ton-my-name.vercel.app/tonconnect-manifest.json`
+
 console.log('Starting app...')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <TonConnectUIProvider 
-    manifestUrl="/tonconnect-manifest.json"
+    manifestUrl={manifestUrl}
     connectRequestParameters={{
       state: 'ready',
       permissions: ['ton_addr']
@@ -19,6 +22,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     }}
     walletsListConfiguration={{
       includeWallets: ['tonkeeper', 'openmask', 'tonhub']
+    }}
+    actionsConfiguration={{
+      twaReturnUrl: 'https://ton-my-name.vercel.app'
     }}
   >
     <App />
